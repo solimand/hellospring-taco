e6abedc3dc62fab58b8a7d9c3b8f155bd47e8eac
msolimando <michele.solimando@unibo.it>
2022-11-23 15:34:18 +0100

    Initial Commit

---
+++ src/main/java/com/tutorial/hellospringtaco/HellospringTacoApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.hellospringtaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringTacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringTacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/hellospringtaco/HellospringTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.hellospringtaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
