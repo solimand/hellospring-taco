9569311bf4d556e7101d4802dc7ec16acdad50f9
solimand <m.solimand@gmail.com>
2022-11-24 14:05:26 +0100

    Lmbok dependecy added

---
+++ src/main/java/com/tutorial/hellospringtaco/Ingredient.java
@@ -0,0 +1,10 @@
+package com.tutorial.hellospringtaco;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    //private final Type type;
+}
