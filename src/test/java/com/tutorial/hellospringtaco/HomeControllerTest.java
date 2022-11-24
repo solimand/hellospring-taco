3af7bc67d3fede5714928e29f8e24041a43047fc
solimand <m.solimand@gmail.com>
2022-11-24 12:29:05 +0100

    Homepage controller test

--- src/test/java/com/tutorial/hellospringtaco/HomeControllerTest.java
+++ src/test/java/com/tutorial/hellospringtaco/HomeControllerTest.java
@@ -1,8 +1,32 @@
 package com.tutorial.hellospringtaco;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
-    
+
+    @Autowired
+    private MockMvc mymock;
+
+    /**
+     * We will test the status 200 of the paghe "/", the name of the view ("home"), and the content ("welcome to...")
+     * @throws Exception
+     */
+    @Test
+    public void testHomePage() throws Exception{
+        mymock.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(containsString("Welcome to...")));
+    }
 }
