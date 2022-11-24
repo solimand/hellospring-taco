7103d07dc7ea3ab83564451ecbc533d8edbc78dd
solimand <m.solimand@gmail.com>
2022-11-24 14:12:18 +0100

    Added TacoOrder to the model

---
+++ src/main/java/com/tutorial/hellospringtaco/TacoOrder.java
@@ -0,0 +1,19 @@
+package com.tutorial.hellospringtaco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class TacoOrder {
+    private String userName;
+    private String userAddr;
+    private String ccNum;
+    
+    private List<Taco> tacos = new ArrayList<>();
+    
+    public void addTaco(Taco taco){
+        this.tacos.add(taco);
+    }
+}
